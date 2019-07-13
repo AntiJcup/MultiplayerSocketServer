79e4f883915b9394b223ef67a4248014446a94fd
AntiJcup <jacobkyle99@yahoo.com>
2019-07-13 09:01:27 -0700

    Disable gamelift in debug mode

--- MultiplayerSocketServer/GameLiftManager.cpp
+++ MultiplayerSocketServer/GameLiftManager.cpp
@@ -1,8 +1,12 @@
 #include "GameLiftManager.h"
 
+#ifndef _DEBUG
 #include <aws/gamelift/server/GameLiftServerAPI.h>
+#endif 
 
 void GameLiftManager::Initialize(int port)
 {
+#ifndef _DEBUG
 	auto t = Aws::GameLift::Server::InitSDK();
+#endif
 }
